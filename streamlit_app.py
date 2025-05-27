4be5812f1daf40ba1d2fb0f7cb2924e0abc39743
Virna2025 <virnanadhiva@gmail.com>
2025-05-27 09:16:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.title("Selamat Datang Di Web Informatika")
+st.title("Selamat Datang Di Web Ghibli Informatika")
 st.write(
     "ngodingseru bersama Virna Nadhiva"
 )
-st.image ("IMG_7321.jpeg")
+st.image("IMG_7321.jpeg")
